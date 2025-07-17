3fa22b39a3b25914e7acf4a89e9c08d3bf120e17
Marcus <virus9320@gmail.com>
2025-07-17 11:08:07 +0700

    Change to vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
-import React from 'react';
-import './App.css';
-import Roulette from './Roulette';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="App h-screen bg-gray-900 text-white">
-      <header className="App-header h-16 flex items-center justify-start bg-gray-800 shadow-md">
-        <Roulette />
-      </header>
-      
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
